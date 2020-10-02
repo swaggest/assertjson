a4497f149bed96efb3f762aafb46469f38bfc76a
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2020-10-02 13:24:19 +0200

    Improve documentation (#4)

--- compact.go
+++ compact.go
@@ -16,16 +16,18 @@ func MarshalIndentCompact(v interface{}, prefix, indent string, lineLen int) ([]
 		return nil, err
 	}
 
+	// Return early if document is small enough.
 	if len(b) <= lineLen {
 		return b, nil
 	}
 
 	m := orderedmap.New()
 
-	// Creating a temporary JSON object to make sure it can be unmarshaled into a map.
+	// Create a temporary JSON object to make sure it can be unmarshaled into a map.
 	tmpMap := append([]byte(`{"t":`), b...)
 	tmpMap = append(tmpMap, '}')
 
+	// Unmarshal JSON payload into ordered map to recursively walk the document.
 	err = json.Unmarshal(tmpMap, m)
 	if err != nil {
 		return nil, err
@@ -36,46 +38,60 @@ func MarshalIndentCompact(v interface{}, prefix, indent string, lineLen int) ([]
 		return nil, orderedmap.NoValueError
 	}
 
-	return marshalIndentCompact(i, indent, append([]byte(prefix), []byte(indent)...), lineLen)
+	// Create first level padding.
+	pad := append([]byte(prefix), []byte(indent)...)
+
+	// Call recursive function to walk the document.
+	return marshalIndentCompact(i, indent, pad, lineLen)
 }
 
-func marshalIndentCompact(m interface{}, indent string, pad []byte, lineLen int) ([]byte, error) {
-	compact, err := json.Marshal(m)
+func marshalIndentCompact(doc interface{}, indent string, pad []byte, lineLen int) ([]byte, error) {
+	// Build compact JSON for provided sub document.
+	compact, err := json.Marshal(doc)
 	if err != nil {
 		return nil, err
 	}
 
+	// Return compact if it fits line length limit with current padding.
 	if len(compact)+len(pad) <= lineLen {
 		return compact, nil
 	}
 
-	switch o := m.(type) {
+	// Indent arrays and objects that are too big.
+	switch o := doc.(type) {
 	case orderedmap.OrderedMap:
 		return marshalObject(o, len(compact), indent, pad, lineLen)
 	case []interface{}:
 		return marshalArray(o, len(compact), indent, pad, lineLen)
 	}
 
+	// Use compact for scalar values (numbers, strings, booleans, nulls).
 	return compact, nil
 }
 
 func marshalArray(o []interface{}, compactLen int, indent string, pad []byte, lineLen int) ([]byte, error) {
+	// Allocate result with a size of compact form, because it is impossible to make result shorter.
 	res := append(make([]byte, 0, compactLen), '[', '\n')
 
 	for i, val := range o {
+		// Build item value with an increased padding.
 		jsonVal, err := marshalIndentCompact(val, indent, append(pad, []byte(indent)...), lineLen)
 		if err != nil {
 			return nil, err
 		}
 
+		// Add item JSON with current padding.
 		res = append(res, pad...)
 		res = append(res, jsonVal...)
 
 		if i == len(o)-1 {
+			// Close array at last item.
 			res = append(res, '\n')
+			// Strip one indent from a closing bracket.
 			res = append(res, pad[len(indent):]...)
 			res = append(res, ']')
 		} else {
+			// Add colon and new line after an item.
 			res = append(res, ',', '\n')
 		}
 	}
@@ -84,8 +100,10 @@ func marshalArray(o []interface{}, compactLen int, indent string, pad []byte, li
 }
 
 func marshalObject(o orderedmap.OrderedMap, compactLen int, indent string, pad []byte, lineLen int) ([]byte, error) {
+	// Allocate result with a size of compact form, because it is impossible to make result shorter.
 	res := append(make([]byte, 0, compactLen), '{', '\n')
 
+	// Iterate object using keys slice to preserve properties order.
 	keys := o.Keys()
 	for i, k := range keys {
 		val, ok := o.Get(k)
@@ -93,26 +111,33 @@ func marshalObject(o orderedmap.OrderedMap, compactLen int, indent string, pad [
 			return nil, orderedmap.NoValueError
 		}
 
+		// Build item value with an increased padding.
 		jsonVal, err := marshalIndentCompact(val, indent, append(pad, []byte(indent)...), lineLen)
 		if err != nil {
 			return nil, err
 		}
 
+		// Marshal key as JSON string.
 		kj, err := json.Marshal(k)
 		if err != nil {
 			return nil, err
 		}
 
+		// Add key JSON with current padding.
 		res = append(res, pad...)
 		res = append(res, kj...)
 		res = append(res, ':')
+		// Add value JSON to the same line.
 		res = append(res, jsonVal...)
 
 		if i == len(keys)-1 {
+			// Close object at last property.
 			res = append(res, '\n')
+			// Strip one indent from a closing bracket.
 			res = append(res, pad[len(indent):]...)
 			res = append(res, '}')
 		} else {
+			// Add colon and new line after a property.
 			res = append(res, ',', '\n')
 		}
 	}
